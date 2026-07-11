bc1ca0999297570df1dd94a4bbe0341124257e14
ViolaBanafsheh <esmailzadehbanafsheh@gmail.com>
2026-07-11 16:14:35 +0200

    Result class part and test if it is correct

--- result.py
+++ result.py
@@ -1,1 +1,10 @@
 # For Storing the current result value
+class Result:
+    def __init__(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, value):
+        self.value = value
---
+++ tests/test_result.py
@@ -0,0 +1,11 @@
+# Here we test the Result class
+
+from result import Result
+
+result = Result(7)
+
+print(result.get_value())
+
+result.set_value(14)
+
+print(result.get_value())

9ba2f527a385fdb2b7df80fc73e55e836472e69d
ViolaBanafsheh <esmailzadehbanafsheh@gmail.com>
2026-07-11 16:25:14 +0200

    Class that defines rules for other classes

--- command.py
+++ command.py
@@ -2,3 +2,13 @@
 # I create a class that defines rules for other classes
 # execute: Should Execute the command and update the result
 # undo: Should undo the command and update the result
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self):
+        pass
+
+    @abstractmethod
+    def undo(self):
+        pass
